340da5c93994a2cc242642ec35924641207ec9ad
Szymon Kotlowski <toushirouu@gmail.com>
2019-05-25 22:06:06 +0200

    spoj

---
+++ SPOJ/Dodawnie liczb całkowitych/main.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+
+using namespace std;
+int liczba1, liczba2, liczba3, wynik;
+int main()
+{
+    cin >> liczba1;
+    cin >> liczba2;
+    cin >> liczba3;
+    wynik = liczba1 + liczba2 + liczba3;
+    cout << wynik << endl;
+    return 0;
+}
