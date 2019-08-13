0cf4a4f360f1d807863122297d1c5e6812c97537
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-13 21:55:24 +0200

    c# another app

---
+++ C#/Course/Variables/Variables.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace App
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string characterName = "John";
+            int characterAge;
+            characterAge = 25;
+            Console.WriteLine("Here once wasa a man named " + characterName);
+            Console.WriteLine("He was " + characterAge + " years old");
+            characterName = "Martin";
+            Console.WriteLine("He really liked the name " + characterName);
+            Console.WriteLine("But didn't like being " + characterAge);
+
+            Console.ReadKey();
+
+        }
+    }
+}
