f8068fde972e7642aafea992ee3ec4eede10aa24
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 23:21:37 +0200

    C# Another If Statement

---
+++ C#/Course/Another If Statement/Another If Statement/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Another_If_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(GetMax(6, 6));
+        }
+
+        static int GetMax(int num1, int num2)
+        {
+            int result;
+
+
+            if (num1 > num2)
+            {
+                result = num1;
+            }
+            else 
+            {
+                result = num2;
+            }
+            
+          
+                
+
+            return result;
+        }
+    }
+}
