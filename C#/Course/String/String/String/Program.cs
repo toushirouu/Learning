728af9f25b61557862a52d7350d3738ebb5ddc3d
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 08:17:52 +0200

    C# strings

---
+++ C#/Course/String/String/String/Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace String
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string phrase = "Giraffe Academy";
+            Console.WriteLine(phrase);
+            Console.WriteLine(phrase[0]);
+            Console.WriteLine(phrase.Length);
+            Console.WriteLine(phrase.ToUpper());
+            Console.WriteLine(phrase.ToLower());
+            Console.WriteLine(phrase.Contains("Academy"));
+            Console.WriteLine(phrase.Contains("Academies"));
+            Console.WriteLine(phrase.IndexOf("Academy"));
+            Console.WriteLine(phrase.IndexOf('c'));
+            Console.WriteLine(phrase.Substring(8));
+            Console.WriteLine(phrase.Substring(8,5));
+
+            Console.ReadKey();
+        }
+    }
+}
