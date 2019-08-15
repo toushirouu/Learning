40c2693fdadadb15099d4ba94c0283d37f20b741
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 20:36:18 +0200

    C# If Statement

---
+++ C#/Course/If statement/If statement/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            bool isMale = true;
+            bool isTall = true;
+
+            if (isMale && isTall)
+            {
+                Console.WriteLine("You are a male and you are tall");
+            }
+            else if (isMale &&!isTall)
+            {
+                Console.WriteLine("You are a male but you aren't tall");
+            }
+            else if (!isMale && isTall)
+            {
+                Console.WriteLine("You aren't male but you are tall");
+            }
+            else
+            {
+                Console.WriteLine("You aren't male and you aren't tall");
+            }
+        }
+    }
+}
