b9d6e2ae8eb8a5bf4a80f3db55a208abe5ddd9ce
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 16:58:20 +0200

    C# course inputs

---
+++ C#/Course/User_Input/User_Input/User_Input/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace User_Input
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter your name: ");
+            string name = Console.ReadLine();
+            Console.Write("Enter your age: ");
+            string age = Console.ReadLine();
+            Console.WriteLine("Hello " + name + " you are " + age + " years old");
+            Console.ReadLine();
+            
+        }
+    }
+}
