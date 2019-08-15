b15321093d0708785217cd7683aba13aae0b8b44
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 20:00:25 +0200

    C# Return Statement

---
+++ C#/Course/Return Statement/Return Statement/Program.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Return_Statement
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.WriteLine(cube(5)); // albo int wynik = cube(5);
+                                        //      Console.WriteLine(wynik);
+        }
+
+        static int cube (int number)
+        {
+            int result = number * number;
+            return result;
+        }
+    }
+}
