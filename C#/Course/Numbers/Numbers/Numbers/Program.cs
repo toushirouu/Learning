cd08d1d04654b3cc9f20ab15f5339497a00e5fb1
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 08:36:15 +0200

    C# strings

---
+++ C#/Course/Numbers/Numbers/Numbers/Program.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Numbers
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int num = 6;
+            num++;
+            num--;
+            Console.WriteLine(-5.78 + 3.23);
+            Console.WriteLine(5 / 2);
+            Console.WriteLine(5 % 2);
+            Console.WriteLine(4 + 2 * 3);
+            Console.WriteLine(( 4 + 2) * 3);
+            Console.WriteLine(5 / 2.0);
+
+            Console.WriteLine(Math.Abs(-40));
+            Console.WriteLine(Math.Pow(3, 2));
+            Console.WriteLine(Math.Sqrt(36));
+            Console.WriteLine(Math.Max(3,30));
+            Console.WriteLine(Math.Min(3, 30));
+            Console.WriteLine(Math.Round(4.3));
+            Console.WriteLine(Math.Round(4.6));
+
+
+            Console.ReadLine();
+        }
+    }
+}
