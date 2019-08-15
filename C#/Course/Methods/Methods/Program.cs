8dec3a8aba6d96e4ffd018853efc859973de3daa
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 19:35:30 +0200

    C# Methods

---
+++ C#/Course/Methods/Methods/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Methods
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SayHi("Mike", 32);
+            SayHi("John", 54);
+            SayHi("Jim", 23);
+
+            Console.ReadLine();
+        }
+
+        static void SayHi(string name, int age)
+        {
+            Console.WriteLine("Hello " + name + " you are " +age);
+
+
+        }
+    }
+}
