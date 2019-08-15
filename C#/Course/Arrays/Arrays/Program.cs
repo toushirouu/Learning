d6cac0b293ee55f843cb64b8add54bf3e1db32f6
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 17:50:13 +0200

    C# arrays

---
+++ C#/Course/Arrays/Arrays/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Arrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            Console.WriteLine(luckyNumbers[0]);
+            luckyNumbers[0] = 32;
+            Console.WriteLine(luckyNumbers[0]);
+
+            string[] friends = { "Mike", "Bob", "John" };
+            Console.WriteLine(friends[0]);
+          
+
+            int[] numbers = new int[3];
+            numbers[0] = 4;
+            numbers[1] = 3;
+            numbers[2] = 55;
+            Console.WriteLine(numbers[0] + numbers[1]+ numbers[2]);
+
+            Console.ReadLine();
+
+
+        }
+    }
+}
