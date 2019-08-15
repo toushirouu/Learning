c2706a6031e5c4c33a63773cdad2060bf3cba262
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 17:17:28 +0200

    C# simple calculator

--- C#/Course/Calculator/Calculator/Calculator/Program.cs
+++ C#/Course/Calculator/Calculator/Calculator/Program.cs
@@ -10,6 +10,14 @@ namespace Calculator
     {
         static void Main(string[] args)
         {
+            Console.Write("Enter a number: ");
+            double num1 = Convert.ToDouble(Console.ReadLine());
+            Console.Write("Enter another number: ");
+            double num2 = Convert.ToDouble(Console.ReadLine());
+            Console.WriteLine(num1 + num2);
+
+
+            Console.ReadLine();
         }
     }
 }
