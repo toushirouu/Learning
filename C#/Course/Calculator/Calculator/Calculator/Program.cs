b51ebc934dd2e8af865a47ea55faacb7476896e1
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-15 17:04:28 +0200

    C# course inputs

--- C#/Course/Calculator/Calculator/Calculator/Program.cs
+++ C#/Course/Calculator/Calculator/Calculator/Program.cs
@@ -4,14 +4,12 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace ConsoleApp1
+namespace Calculator
 {
     class Program
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World");
-            Console.ReadKey();
         }
     }
 }
