6e16869a412c157205231e84904ea93793b2863e
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-16 23:39:53 +0200

    C# If calculator

---
+++ C#/Course/If calculator/If calculator/Program.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace If_calculator
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+        }
+
+        static void Calculator(int num1, int num2)
+        {
+            Console.WriteLine(");
+        }
+    }
+}
