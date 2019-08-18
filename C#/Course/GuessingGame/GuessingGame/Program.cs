10c8e01b22e96790456aebf49c56ea3eecc83a0e
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 16:53:22 +0200

    C# Guessing Game

---
+++ C#/Course/GuessingGame/GuessingGame/Program.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string secretWord = "giraffe";
+            string guess = "";
+            int index = 0;
+            while (index <= 10 && guess != secretWord)
+            {
+                Console.Write("Enter guess: ");
+                guess = Console.ReadLine();
+                index++;
+            }
+            if (guess ==  secretWord)
+            {
+               Console.WriteLine("You Win!");
+               Console.WriteLine("You won in " + index + " try");
+            }
+            else
+            {
+                Console.WriteLine("You lose :(");
+            }
+            
+        }
+    }
+}
