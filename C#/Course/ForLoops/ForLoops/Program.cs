007fc084944522e6565c51dee9283a58fde60004
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 19:10:07 +0200

    C# for loop

---
+++ C#/Course/ForLoops/ForLoops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ForLoops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] luckyNumbers = { 4, 8, 15, 16, 23, 42 };
+            for (int i = 0; i < luckyNumbers.Length; i++)
+            {
+                Console.WriteLine(luckyNumbers[i]);
+
+            }
+            Console.ReadLine();
+        }
+    }
+}
