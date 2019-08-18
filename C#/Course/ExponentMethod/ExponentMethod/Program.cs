0814b3bc2c99d67420a6fbcb492c02e97f75c9ac
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 19:50:30 +0200

    C# Exponent Method #2

--- C#/Course/ExponentMethod/ExponentMethod/Program.cs
+++ C#/Course/ExponentMethod/ExponentMethod/Program.cs
@@ -10,6 +10,20 @@ namespace ExponentMethod
     {
         static void Main(string[] args)
         {
+            Console.WriteLine(GetPow(2,3));
+        }
+        
+        static int GetPow(int baseNum, int powNum)
+        {
+            int result = 1;
+
+            for (int i = 0; i < powNum; i++)
+            {
+                result = result * baseNum;
+
+            }
+
+            return result;
         }
     }
 }
