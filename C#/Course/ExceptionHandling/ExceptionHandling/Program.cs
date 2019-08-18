8a1c9cb671ce6488393765076b0fb970ee4b7604
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 20:20:49 +0200

    C# Exceptions

---
+++ C#/Course/ExceptionHandling/ExceptionHandling/Program.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ExceptionHandling
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        
+            try
+            {
+                Console.Write("Type first number: ");
+                int num1 = Convert.ToInt32(Console.ReadLine());
+                Console.Write("Type first number: ");
+                int num2 = Convert.ToInt32(Console.ReadLine());
+                int result;
+                Console.WriteLine(result = (num1 / num2));
+
+            }
+
+            catch (Exception s)                     // mozna uzyc tez wybranych wyjatkow, np. DivideByZeroException, FormatException
+            {
+                Console.WriteLine(s.Message);       
+
+            }
+            
+
+           
+
+        }
+    }
+}
