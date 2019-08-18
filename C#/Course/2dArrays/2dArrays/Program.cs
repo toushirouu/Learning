1f0fd6351b378381acfe01884fe867ffae46afc2
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 19:58:50 +0200

    C# 2d Arrays

---
+++ C#/Course/2dArrays/2dArrays/Program.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace _2dArrays
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int[,] myArray =
+            {
+                { 3,2 },
+                { 4,1 },
+                { 12,3}
+            };
+            int[,] nextArray = new int[2, 3]; // ilosc rzedow i kolum
+
+            Console.WriteLine(myArray[0,0]);
+            Console.WriteLine(myArray[2,0]);
+
+            Console.ReadLine();
+        }
+    }
+}
