7da4b275681e4707616b938ba1c3fa80f897eda0
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-18 17:31:06 +0200

    C# Guessing Game with do while loop

---
+++ C#/Course/GuessingGame_DoWhile/GuessingGame_DoWhile/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GuessingGame_DoWhile
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            string guessingWord = "giraffe";
+            string guess = "";
+            int guessingCount = 0;
+            int guessingMax = 3;
+
+            bool outOfGuessing = false;
+
+            do
+            {
+                if (guessingCount < guessingMax)
+                {
+                    Console.Write("Enter guess: ");
+                    guess = Console.ReadLine();
+                    guessingCount++;
+                }
+
+                else
+                {
+                    outOfGuessing = true;
+
+                }
+            }
+
+            while (guess != guessingWord && outOfGuessing == false);
+
+            if (guess == guessingWord)
+            {
+                 Console.WriteLine("You Win!");
+                 Console.WriteLine("You won in " + guessingCount + " try" );
+            }
+
+            else
+            {
+                Console.WriteLine("You lose");
+            }
+            
+        }
+    }
+}
