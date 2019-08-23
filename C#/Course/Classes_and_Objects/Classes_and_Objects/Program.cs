93552b976cbef602acecbb0508158e8da3f28e12
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-23 18:55:20 +0200

    C# classes and objects #1

--- C#/Course/Classes_and_Objects/Classes_and_Objects/Program.cs
+++ C#/Course/Classes_and_Objects/Classes_and_Objects/Program.cs
@@ -10,6 +10,22 @@ namespace Classes_and_Objects
     {
         static void Main(string[] args)
         {
+            Book book1 = new Book();
+            Book book2 = new Book();
+
+            book1.title = "Harry Potter";
+            book1.author = "JK Rowling";
+            book1.pages = 400;
+
+            book2.title = "Lord of the Rings";
+            book2.author = "Tolkien";
+            book2.pages = 700;
+
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book2.title);
+
+            Console.ReadLine();
+
         }
     }
 }
