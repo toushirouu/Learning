449014a727dad29ab336576a0fbc572fc08d251c
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-23 19:23:39 +0200

    C# Constructors #1

---
+++ C#/Course/Constructor/Constructor/Book.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Book
+    {
+        public string title;
+        public string author;
+        public int pages;
+
+        public Book(string aTitle, string aAuthor, int aPages)
+        {
+
+            aTitle = Console.ReadLine();
+            aAuthor = Console.ReadLine();
+            aPages = Convert.ToInt32(Console.ReadLine());
+            title = aTitle;
+            author = aAuthor;
+            pages = aPages;
+
+
+        }
+    }
+}
+
+    
---
+++ C#/Course/Constructor/Constructor/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Constructor
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("loooool");
+            Book book1 = new Book("","",0);
+            Console.WriteLine(book1.author);
+            Console.WriteLine(book1.pages);
+        }
+    }
+}
