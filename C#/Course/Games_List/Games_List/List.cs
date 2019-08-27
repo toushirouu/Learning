e31da8282df1886719fe0274e7c96d814a9f93a6
Szymon Kotlowski <toushirouu@gmail.com>
2019-08-27 20:04:03 +0200

    Another Class and Constructor

---
+++ C#/Course/Games_List/Games_List/List.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_List
+{
+    class List
+    {
+        public string title;
+        public string producer;
+        public string type;
+
+        public List(string aTitle, string aProducer, string aType)
+        {
+         
+
+            title = aTitle;
+            producer = aProducer;
+            type = aType;
+
+        }
+    }
+}
---
+++ C#/Course/Games_List/Games_List/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Games_List
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            List game1 = new List("The Witcher", "CD Projekt RED", "RPG");
+            Console.WriteLine(game1.title);
+            Console.WriteLine(game1.producer);
+            Console.WriteLine(game1.type);
+
+            Console.WriteLine();
+
+            List game2 = new List("World of Warcraft", "Blizzard Entertainment", "MMORPG");
+            Console.WriteLine(game2.title);
+            Console.WriteLine(game2.producer);
+            Console.WriteLine(game2.type);
+
+            Console.ReadLine();
+        }
+    }
+}
