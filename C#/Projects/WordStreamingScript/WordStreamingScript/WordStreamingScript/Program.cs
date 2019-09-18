99267210a57d6043c890c2dc6481129aaac079d0
Szymon Kotlowski <toushirouu@gmail.com>
2019-09-18 19:29:35 +0200

    C# reading xml script

--- C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
+++ C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Xml;
 using System.Collections.Generic;
 using System.Collections;
 using System.Linq;
@@ -12,8 +13,28 @@ namespace WordStreamingScript
     {
         static void Main(string[] args)
         {
-            
-            string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "cos*.xml");
+            string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "str*.xml");
+            foreach (string file in files)
+            {
+                ProcessFile(file);
+            }
+            for (int i = 0; i < files.Length; i++)
+            {
+                Console.WriteLine(files[i]);
+                XmlDocument xml = new XmlDocument();
+                xml.Load(files[i]);
+
+                XmlNodeList xnList = xml.SelectNodes("/Names/Name");
+                foreach (XmlNode xn in xnList)
+                {
+                    using (StreamWriter StreamW = new StreamWriter((@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug\lol.txt"), true))
+                    {
+                        StreamW.WriteLine(xn.InnerText);
+                    }
+                }
+            }
+
+            /*string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "cos*.xml");
             foreach (string file in files)
             {
                 ProcessFile(file);
@@ -31,14 +52,17 @@ namespace WordStreamingScript
                         while ((oneLine = StreamR.ReadLine()) != null)
                         {
                             StreamW.WriteLine(oneLine);
+                            
                         }
                     }
                 }
             }
+        }*/
         }
         public static void ProcessFile(string path)
         {
             Console.WriteLine("Processed file '{0}',", path);
         }
+        }
     }
-}
+
