439c0759de8fa1435f5c9405560814a31bf017fb
Szymon Kotlowski <toushirouu@gmail.com>
2019-09-18 20:28:43 +0200

    C# reading xml script

--- C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
+++ C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
@@ -27,42 +27,22 @@ namespace WordStreamingScript
                 XmlNodeList xnList = xml.SelectNodes("/Names/Name");
                 foreach (XmlNode xn in xnList)
                 {
+
                     using (StreamWriter StreamW = new StreamWriter((@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug\lol.txt"), true))
                     {
                         StreamW.WriteLine(xn.InnerText);
                     }
                 }
             }
+            File.WriteAllLines("lol1.txt", File.ReadAllLines("lol.txt").Distinct());
 
-            /*string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "cos*.xml");
-            foreach (string file in files)
-            {
-                ProcessFile(file);
-            }
-
-            for (int i = 0; i < files.Length; i++)
-            {
-                using (StreamReader StreamR = new StreamReader(files[i]))
-                {
-                    string oneLine;
-
-                    using (StreamWriter StreamW = new StreamWriter((@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug\czyt.txt"), true))
-                    {
-
-                        while ((oneLine = StreamR.ReadLine()) != null)
-                        {
-                            StreamW.WriteLine(oneLine);
-                            
-                        }
-                    }
-                }
-            }
-        }*/
         }
         public static void ProcessFile(string path)
         {
             Console.WriteLine("Processed file '{0}',", path);
         }
-        }
+
+
     }
+}
 
