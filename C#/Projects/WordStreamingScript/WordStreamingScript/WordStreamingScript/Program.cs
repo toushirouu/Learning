0bd29cbe8c4bc1d109b9abe79e2a2edbae4bab00
Szymon Kotlowski <toushirouu@gmail.com>
2019-09-19 22:26:12 +0200

    C# project Update

--- C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
+++ C#/Projects/WordStreamingScript/WordStreamingScript/WordStreamingScript/Program.cs
@@ -6,6 +6,7 @@ using System.Linq;
 using System.Text;
 using System.IO;
 using System.Threading.Tasks;
+using System.Xml.Linq;
 
 namespace WordStreamingScript
 {
@@ -13,35 +14,62 @@ namespace WordStreamingScript
     {
         static void Main(string[] args)
         {
-            string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "str*.xml");
+            int num;
+            string[] files = Directory.GetFiles(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug", "*.xml");
             foreach (string file in files)
             {
                 ProcessFile(file);
             }
+            Console.WriteLine("Zrzucenie nazw obiektów do txt - wciśnij 1");
+            Console.WriteLine("Zamiana nazw obiektów - wciśnij 2");
+            num = Convert.ToInt32(Console.ReadLine());
             for (int i = 0; i < files.Length; i++)
             {
+
                 Console.WriteLine(files[i]);
                 XmlDocument xml = new XmlDocument();
                 xml.Load(files[i]);
-
                 XmlNodeList xnList = xml.SelectNodes("/Names/Name");
-                foreach (XmlNode xn in xnList)
-                {
 
-                    using (StreamWriter StreamW = new StreamWriter(("lol.txt"), true))
-                    {
-                        StreamW.WriteLine(xn.InnerText);
-                    }
+                
+                switch (num)
+
+                {
+                    case 1:
+                        foreach (XmlNode xn in xnList)
+                        {
+                            using (StreamWriter StreamW = new StreamWriter(("temp.txt"), true))
+
+                            {
+                                StreamW.WriteLine(xn.InnerText);
+                            }
+                        }
+                        File.WriteAllLines("ClassNames.txt", File.ReadAllLines("temp.txt").Distinct());
+                       
+                        break;
+
+
+                    case 2:
+
+                        var doc = XDocument.Load(files[i]);   
+                        var elementsToUpdate = doc.Descendants()
+                                                  .Where(o => o.Value == "Szymon" && !o.HasElements);
+
+                        foreach (XElement element in elementsToUpdate)
+                        {
+                            element.Value = "new object name";
+                        }
+  
+                        doc.Save(files[i]);
+                        break;
                 }
             }
-            File.WriteAllLines("lol1.txt", File.ReadAllLines("lol.txt").Distinct());
-
+            File.Delete(@"E:\Learning\C#\Projects\WordStreamingScript\WordStreamingScript\WordStreamingScript\bin\Debug\temp.txt");
         }
         public static void ProcessFile(string path)
         {
             Console.WriteLine("Processed file '{0}',", path);
         }
-
 
     }
 }
